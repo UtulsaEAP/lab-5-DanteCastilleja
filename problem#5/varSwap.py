ac4f4451ec294ab34ea08715632ab067ad9d4cb6
DanteCastilleja <dsc2386@utulsa.edu>
2024-02-29 23:34:11 -0600

    Fixed

--- problem#5/varSwap.py
+++ problem#5/varSwap.py
@@ -1,9 +1,8 @@
 def swap_values(user_val1, user_val2, user_val3, user_val4):   
    #write your code here
-   user_val1 = user_input2
-   user_val2 = user_input1
-   user_val3 = user_input4
-   user_val4 = user_input3
+   user_val1, user_val2 = user_val2, user_val1
+   user_val3, user_val4 = user_val4, user_val3
+   
    return user_val1, user_val2, user_val3, user_val4
 
 if __name__ == '__main__':   
@@ -13,5 +12,5 @@ if __name__ == '__main__':
    user_input4 = int(input())
    #store output for every input here
    #print those output
-   swapped = swap_values(user_input2, user_input1, user_input4, user_input3)
+   swapped = swap_values(user_input1, user_input2, user_input3, user_input4)
    print(*swapped)
