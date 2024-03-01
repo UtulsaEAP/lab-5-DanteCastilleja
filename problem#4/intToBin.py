c79aa21ab6d6744220bcdbc02bc85301dc732b68
DanteCastilleja <dsc2386@utulsa.edu>
2024-02-29 22:14:39 -0600

    Problem 5

--- problem#4/intToBin.py
+++ problem#4/intToBin.py
@@ -2,16 +2,20 @@ def int_to_reverse_binary(num1):
     binary_val = ''
 #write your while loop here
     while num1 > 0:
-        #write your code
-
-    return binary_val;
-
-
+        if num1 % 2 == 0 :
+            binary_val += '0'
+            num1 //= 2
+        elif num1 % 2 != 0 :
+            binary_val += '1'
+            num1 //= 2
+        elif num1 <= 1 :
+            binary_val += '1'
+    return binary_val
 def string_reverse(input_string): 
     reverse_input = ''
-    
    #write your for loop here
-    
+    for char in input_string:
+        reverse_input = char + reverse_input
     return reverse_input
 
 if __name__ == '__main__':
--- problem#5/varSwap.py
+++ problem#5/varSwap.py
@@ -1,6 +1,9 @@
 def swap_values(user_val1, user_val2, user_val3, user_val4):   
    #write your code here
-   
+   user_val1 = user_input2
+   user_val2 = user_input1
+   user_val3 = user_input4
+   user_val4 = user_input3
    return user_val1, user_val2, user_val3, user_val4
 
 if __name__ == '__main__':   
@@ -10,5 +13,5 @@ if __name__ == '__main__':
    user_input4 = int(input())
    #store output for every input here
    #print those output
-
- 
+   swapped = swap_values(user_input2, user_input1, user_input4, user_input4)
+   print(*swapped)
